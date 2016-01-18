9b9c82df8e4d47d25ed9cdf090082db3c9d9b08d
Prabhakar Gupta <prabhakargupta267@gmail.com>
2016-01-19 01:39:10 +0530

    forms rendered using django form

--- R_hire/forms.py
+++ R_hire/forms.py
@@ -2,19 +2,19 @@
 # -*- coding: utf-8 -*-
 # @Author: sahildua2305
 # @Date:   2016-01-18 22:57:52
-# @Last Modified by:   sahildua2305
-# @Last Modified time: 2016-01-18 23:39:44
+# @Last Modified by:   Prabhakar Gupta
+# @Last Modified time: 2016-01-19 01:36:29
 
 from django import forms
 
 
 class RegistrationForm(forms.Form):
-	fname = forms.CharField(label='First name', max_length=200)
-	lname = forms.CharField(label='Last name', max_length=200)
+	fname = forms.CharField(label='First name', max_length=200, widget=forms.TextInput(attrs={'class' : 'form-control'}))
+	lname = forms.CharField(label='Last name', max_length=200, widget=forms.TextInput(attrs={'class' : 'form-control'}), required=False)
 
-	email = forms.EmailField(label='Enter your email')
-	password = forms.CharField(widget=forms.PasswordInput, label='Enter your password')
-	password2 = forms.CharField(widget=forms.PasswordInput, label='Confirm password')
+	email = forms.EmailField(label='Enter your email', widget=forms.EmailInput(attrs={'class' : 'form-control'}))
+	password = forms.CharField(widget=forms.PasswordInput(attrs={'class' : 'form-control'}), label='Enter your password')
+	password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class' : 'form-control', 'placeholder' : 'Should be same as password field, obviously'}), label='Confirm password')
 
 	# clean_<fieldname> method in a form class is used to do custom validation for the field.
 	# We are doing a custom validation for the 'password2' field and raising
