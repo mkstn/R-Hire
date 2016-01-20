ed2825dc79279c37617d421cda5a7da69ff246e3
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2016-01-21 03:43:58 +0530

    login added

--- R_hire/views.py
+++ R_hire/views.py
@@ -3,18 +3,17 @@
 # @Author: Sahil Dua
 # @Date:   2016-01-08 22:48:10
 # @Last Modified by:   Prabhakar Gupta
-# @Last Modified time: 2016-01-21 03:08:17
+# @Last Modified time: 2016-01-21 03:40:33
 
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Import the RegistrationForm class from the forms.py in the same module
+# Import the RegistrationForm, LoginForm classes from the forms.py in the same module
 from .forms import RegistrationForm
+from .forms import LoginForm
 
 # Import Candidate model 
 from .models import Candidate
-
-import json
 
 
 def index(request):
@@ -40,9 +39,7 @@ def register(request):
 
 			# Make an old_candidate object to check whether 
 			# email id has already been used or not
-			old_candidate = Candidate.objects.filter(
-				email = email,
-			)
+			old_candidate = Candidate.objects.filter(email = email)
 
 			# if no candidate has used this email id then register the candidate
 			# else throw an error
@@ -70,3 +67,47 @@ def register(request):
 	# be rendered with the entered data and error messages. Otherwise an empty
 	# form will be rendered.
 	return render(request, "R_hire/registration/registration_form.html", {"form" : form})
+
+
+def login(request):
+	# If the request method is POST, it means that the form has been submitted
+	# and we need to validate it
+	if request.method == "POST":
+		# Create a RegistrationForm instance with the submitted data
+		form = LoginForm(request.POST)
+
+		# is_valid validates a form and returns
+		# True if it is valid and
+		# False if it is invalid
+		if form.is_valid():
+			email 		= request.POST['email']
+			password 	= request.POST['password']
+
+			# Make an check_candidate object to check whether 
+			# email id and password make up a valid combination
+			check_candidate = Candidate.objects.filter(
+				email = email,
+				password = password,
+			)
+
+			# if combination is not correct then throw an error
+			# else login the candidate
+			if (check_candidate.count() != 1) :
+				form.add_error(None, 'Invalid email - password combination')
+			# else:
+				# TO DO
+				# add session control functions here
+				# since candidate exists 
+
+	# This means that the request is a GET request. So we need to
+	# create an instance of the LoginForm class and render it in the template
+	else:
+		form = LoginForm()
+
+	# Render the registration form template with a RegistrationForm instance. If the
+	# form was submitted and the data found to be invalid, the template will
+	# be rendered with the entered data and error messages. Otherwise an empty
+	# form will be rendered.
+	return render(request, "R_hire/registration/login_form.html", {"form" : form})
+
+
